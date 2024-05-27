c8cbfdc72ac373f5032693a788920cb88ffab140
Francisco Javier Gutiérrez Moreno <javigmdev@MacBook-Pro-de-Francisco.local>
2024-05-27 18:35:05 +0200

    Kata: Csv Filter

---
+++ src/core/11-kata-csv-filter/csvFilter.ts
@@ -0,0 +1,73 @@
+export class CsvFilter {
+  constructor(private readonly lines: string[]) {}
+  static create(lines: string[]) {
+    if (lines.length === 1) {
+      throw new Error('Single line is not allowed');
+    }
+    return new CsvFilter(lines);
+  }
+
+  get filteredLines() {
+    if (this.lines.length === 0) {
+      return [];
+    }
+    const header = this.lines[0];
+    const invoices = this.lines.slice(1);
+    return [header].concat(
+      this.takeRepeatedInvoiceIds(this.takeValidInvoices(invoices)),
+    );
+  }
+
+  private takeValidInvoices(invoices: string[]) {
+    return invoices.filter(this.isValidInvoice);
+  }
+
+  private isValidInvoice = invoice => {
+    const fields = invoice.split(',');
+    const ivaField = fields[4];
+    const igicField = fields[5];
+    const decimalRegex = '\\d+(\\.\\d+)?';
+    const areTaxFieldsMutuallyExclusive =
+      (ivaField.match(decimalRegex) || igicField.match(decimalRegex)) &&
+      (!ivaField || !igicField);
+    const grossAmountField = fields[2];
+    const netAmountField = fields[3];
+    const isNetAmountCorrect =
+      this.hasCorrectAmount(netAmountField, grossAmountField, ivaField) ||
+      this.hasCorrectAmount(netAmountField, grossAmountField, igicField);
+    const areIdentifierFieldsMutuallyExclusive = !fields[7] || !fields[8];
+    return (
+      areTaxFieldsMutuallyExclusive &&
+      isNetAmountCorrect &&
+      areIdentifierFieldsMutuallyExclusive
+    );
+  };
+
+  private hasCorrectAmount(
+    netAmountField: string,
+    grossAmountField: string,
+    ivaField: string,
+  ) {
+    const parsedNetAmount = parseFloat(netAmountField);
+    const parsedGrossAmount = parseFloat(grossAmountField);
+    const parsedTaxField = parseFloat(ivaField);
+    return (
+      parsedNetAmount ===
+      parsedGrossAmount - (parsedGrossAmount * parsedTaxField) / 100
+    );
+  }
+
+  private takeRepeatedInvoiceIds(invoices: string[]) {
+    const invoicesIds = invoices.map(invoice => this.invoiceId(invoice));
+    const duplicatedIds = invoicesIds.filter(
+      (id, index) => invoicesIds.indexOf(id) !== index,
+    );
+    return invoices.filter(
+      invoice => !duplicatedIds.includes(this.invoiceId(invoice)),
+    );
+  }
+
+  private invoiceId(invoice: string) {
+    return invoice.split(',')[0];
+  }
+}
---
+++ src/tests/11-kata-csv-filter/csvFilter.test.ts
@@ -0,0 +1,186 @@
+import { CsvFilter } from '../../core/11-kata-csv-filter/csvFilter';
+
+describe('CSV filter', () => {
+  const header =
+    'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+  const emptyField = '';
+
+  it('allows for correct lines only', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({});
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header, invoiceLine]);
+  });
+
+  it('allows only the correct lines when the igic tax is applied', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      ivaTax: '',
+      igicTax: '7',
+      netAmount: '930',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header, invoiceLine]);
+  });
+
+  it('allows only multiple correct lines', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({ invoiceId: '1' });
+    const invoiceLine2 = fileWithOneInvoiceLineHaving({ invoiceId: '2' });
+
+    const csvFilter = CsvFilter.create([header, invoiceLine, invoiceLine2]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header, invoiceLine, invoiceLine2]);
+  });
+
+  it('excludes lines with both tax fields populated as they are exclusive', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      ivaTax: '21',
+      igicTax: '7',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with both tax fields empty as one is required', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      ivaTax: '',
+      igicTax: '',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with non decimal tax fields', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      ivaTax: 'XYZ',
+      igicTax: '',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with both tax fields populated even if non decimal', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      ivaTax: 'XYZ',
+      igicTax: '7',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with miscalculated net amount for iva tax', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      igicTax: '',
+      netAmount: '900',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with miscalculated net amount for igic tax', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({
+      ivaTax: '',
+      igicTax: '7',
+      netAmount: '900',
+    });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with cif and nif fields populated as they are exclusive', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({ nif: 'b76730373' });
+    const csvFilter = CsvFilter.create([header, invoiceLine]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header]);
+  });
+
+  it('excludes lines with repeated invoice id', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({ invoiceId: '1' });
+    const invoiceLine2 = fileWithOneInvoiceLineHaving({ invoiceId: '1' });
+    const invoiceLine3 = fileWithOneInvoiceLineHaving({ invoiceId: '3' });
+    const invoiceLine4 = fileWithOneInvoiceLineHaving({ invoiceId: '4' });
+    const invoiceLine5 = fileWithOneInvoiceLineHaving({ invoiceId: '3' });
+    const csvFilter = CsvFilter.create([
+      header,
+      invoiceLine,
+      invoiceLine2,
+      invoiceLine3,
+      invoiceLine4,
+      invoiceLine5,
+    ]);
+
+    const result = csvFilter.filteredLines;
+
+    expect(result).toEqual([header, invoiceLine4]);
+  });
+
+  it('an empty list will produce an output empty list', () => {
+    const csvFilter = CsvFilter.create([]);
+    const result = csvFilter.filteredLines;
+    expect(result).toEqual([]);
+  });
+
+  it('does not allow a list of invoices with a single line', () => {
+    const invoiceLine = fileWithOneInvoiceLineHaving({});
+    const result = () => CsvFilter.create([invoiceLine]);
+    expect(result).toThrow();
+  });
+
+  interface FileWithOneInvoiceLineHavingParams {
+    invoiceId?: string;
+    ivaTax?: string;
+    igicTax?: string;
+    netAmount?: string;
+    nif?: string;
+  }
+
+  function fileWithOneInvoiceLineHaving({
+    invoiceId = '1',
+    ivaTax = '21',
+    igicTax = emptyField,
+    netAmount = '790',
+    nif = emptyField,
+  }: FileWithOneInvoiceLineHavingParams) {
+    const invoiceDate = '02/05/2019';
+    const grossAmount = '1000';
+    const concept = 'ACER Laptop';
+    const cif = 'B76430134';
+    return [
+      invoiceId,
+      invoiceDate,
+      grossAmount,
+      netAmount,
+      ivaTax,
+      igicTax,
+      concept,
+      cif,
+      nif,
+    ].join(',');
+  }
+});
